ecc268d200679200e6e582d5e140083e58825254
Kandukuri Vishnuraj <k.vishnurajchary@gmail.com>
2023-10-02 12:04:03 +0530

    Added new feature phone number

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -6,9 +6,10 @@
         public MockEmployeeRepository()
         {
             _employees = new List<Employee>() { 
-                new Employee(){Id=1,Name="Robert",Department="IT",Email="Demo123@gmail.com"},
-                new Employee(){Id=2,Name="Angela",Department="IT",Email="Demo233@gmail.com"},
-                new Employee(){Id=3,Name="betty",Department="HR",Email="Demo007@gmail.com"},
+                //Added phone number
+                new Employee(){Id=1,Name="Robert",Department="IT",Email="Demo123@gmail.com",Phonenumber="123456789"},
+                new Employee(){Id=2,Name="Angela",Department="IT",Email="Demo233@gmail.com",Phonenumber="232323232"},
+                new Employee(){Id=3,Name="betty",Department="HR",Email="Demo007@gmail.com",Phonenumber="2323232323"},
             };
         }
 
